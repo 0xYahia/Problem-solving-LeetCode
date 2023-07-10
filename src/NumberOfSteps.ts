70a17c265cab2e4447ba2caa95f5391f2de9daa3
Yahia <mohamedyahia207545@gmail.com>
2023-07-10 15:39:06 +0300

    NumberOfSteps

---
+++ src/NumberOfSteps.ts
@@ -0,0 +1,32 @@
+function numberOfSteps(num: number): number {
+  let steps: number = 0;
+  while (num > 0) {
+    if (num % 2 === 0) {
+      num = num / 2;
+      console.log(
+        `Step ${steps + 1}) ${num * 2} is even; divide by 2 and obtain ${num}. `
+      );
+    } else {
+      num = num - 1;
+      console.log(
+        `Step ${steps + 1}) ${num + 1} is odd; subtract 1 and obtain ${num}. `
+      );
+    }
+    steps += 1;
+  }
+  return steps;
+}
+
+console.log(numberOfSteps(14));
+console.log("########################");
+console.log(numberOfSteps(8));
+console.log("########################");
+console.log(numberOfSteps(123));
+
+// Enhanced
+function numberOfSteps2(num: number): number {
+  for (var i = 0; num != 0; num % 2 === 0 ? (num /= 2) : num--) {
+    i++;
+  }
+  return i;
+}
